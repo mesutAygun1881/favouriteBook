e68b04707fbeb2c14c4921eaedbb0825d7d28ae5
mesutAygun <mesutaygun35.25@gmail.com>
2021-03-28 01:31:46 +0300

    first commit

---
+++ favouriteBook/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  favouriteBook
+//
+//  Created by mesutAygun on 27.03.2021.
+//
+
+import Foundation
---
+++ favouriteBook/detail.swift
@@ -0,0 +1,20 @@
+//
+//  detail.swift
+//  favouriteBook
+//
+//  Created by mesutAygun on 27.03.2021.
+//
+
+import SwiftUI
+
+struct detail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct detail_Previews: PreviewProvider {
+    static var previews: some View {
+        detail()
+    }
+}
