36f8e989ad899aa3388b850638315024bc6a1bd7
mesutAygun <mesutaygun35.25@gmail.com>
2021-03-27 18:29:19 +0300

    Initial Commit

---
+++ favouriteBook/favouriteBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  favouriteBookApp.swift
+//  favouriteBook
+//
+//  Created by mesutAygun on 27.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct favouriteBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
